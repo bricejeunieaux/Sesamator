62aa1ab44c0ceedb1b6ab2cda2c1fbeae4bec170
Brice Jeunieaux <JeunieauxBrice@hotmail.fr>
2018-05-04 17:24:22 +0200

    Version 0.1.0.0 - Ajout de fonctionnalités non-importantes

--- sesamator.sh
+++ sesamator.sh
@@ -16,3 +16,56 @@
 #Licence GNU GPL v3	 : https://www.gnu.org/licenses/quick-guide-gplv3.fr.html
 
 
+
+
+
+
+##############################  PARTIE 1 : PARAMÉTRAGE NON-IMPORTANTS	########################################
+#####Ces paramétrages ne jouent pas un grand rôle dans le script (pas d'opérations)
+
+initFenetre() {						#Dimensionnage fenêtre + effaçage
+
+resize -s 35 120
+clear
+
+}
+
+initFenetre ;
+
+definitionCouleur() {					#Fonction pour faciliter la colorisation dans le script
+							#à l'aide de variables contenant les "code-couleurs"
+enDefaut='\033[0m' ;
+
+fgBlanc='\033[37m' ;
+fgCyan='\033[36m' ;
+fgViolet='\033[35m' ;
+fgBleu='\033[34m' ;
+fgJaune='\033[33m' ;
+fgVert='\033[32m' ;
+fgRouge='\033[31m' ;
+fgNoir='\033[30m' ;
+
+bgBlanc='\033[47m' ;
+bgCyan='\033[46m' ;
+bgViolet='\033[45m' ;
+bgBleu='\033[44m' ;
+bgJaune='\033[43m' ;
+bgVert='\033[42m' ;
+bgRouge='\033[41m' ;
+bgNoir='\033[40m' ;
+
+enGras='\033[1m' ;
+enSouligne='\033[4m' ;
+enSurligne='\033[7m' ;
+
+}
+
+definitionCouleur ;
+
+
+
+
+
+
+
+
