efde9d2b0a3d40159851bbf06945758dba6edd96
Brice Jeunieaux <JeunieauxBrice@hotmail.fr>
2018-05-04 16:53:39 +0200

    Version 0.0.0 - Préparation des fichiers Git/Github

---
+++ sesamator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##############################################################  ##############################################################
+#     _____                                 _                #  #                                                            #
+#    / ____|                               | |               #  #             GÉNÉRATEUR SIMPLE DE MOTS DE PASSE             #
+#   | (___   ___  ___  __ _ _ __ ___   __ _| |_ ___  _ __    #  #                                                            #
+#    \___ \ / _ \/ __|/ _` | '_ ` _ \ / _` | __/ _ \| '__|   #  #         Prototype actuel codé en script Bash Linux         #
+#    ____) |  __/\__ \ (_| | | | | | | (_| | || (_) | |      #  #                                                            #
+#   |_____/ \___||___/\__,_|_| |_| |_|\__,_|\__\___/|_|      #  #                  Version uploadée : 0.0.0                  #
+#                                                            #  #                                                            #
+##############################################################  ##############################################################
+
+
+#Auteur du projet source : Jeunieaux Brice	-	JeunieauxBrice@hotmail.fr	brice_jeunieaux@member.fsf.org (XMPP/Pidgin)
+#Github du projet	 : https://github.com/bricejeunieaux/sesamator
+#Licence GNU GPL v3	 : https://www.gnu.org/licenses/quick-guide-gplv3.fr.html
+
+
